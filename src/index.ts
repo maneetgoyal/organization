64f21ef0f321bd9009f92d4a60e83d5dec141361
maneetgoyal <maneetgoyal14@gmail.com>
2023-06-17 00:33:06 +0530

    feat: Add move logic

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Mark } from "./example";
+import { get } from "lodash";
 import type { Employee, IEmployeeOrgApp, Move } from "./types";
 
 export class EmployeeOrgApp implements IEmployeeOrgApp {
@@ -11,7 +12,7 @@ export class EmployeeOrgApp implements IEmployeeOrgApp {
     this.moves = [];
   }
 
-  public findEmployee(currentEmployee: Employee, employeeID: number): string | undefined {
+  private findEmployee(currentEmployee: Employee, employeeID: number): string | undefined {
     let path: string | undefined;
     if (currentEmployee.uniqueID === employeeID) {
       path = "";
@@ -30,8 +31,45 @@ export class EmployeeOrgApp implements IEmployeeOrgApp {
     return path;
   }
 
+  private getSupervisorPath(input?: string): string | undefined {
+    const index = input?.lastIndexOf(".");
+    let path: string | undefined;
+    if (typeof index === "number" && index !== -1) {
+      path = input?.substring(0, index);
+    }
+    return path;
+  }
+
   public move(employeeID: number, supervisorID: number): void {
     const newMove: Move = { employeeID, newSupervisorID: supervisorID, oldSupervisorID: supervisorID };
+    const employeePath = this.findEmployee(this.ceo, employeeID);
+    if (typeof employeePath === "string") {
+      const employee = get(this.ceo, employeePath) as Employee | undefined;
+      if (employee !== undefined) {
+        // Change the supervisor of the employee's subordinates
+        const oldSupervisorPath = this.getSupervisorPath(employeePath);
+        if (typeof oldSupervisorPath === "string") {
+          const oldSupervisor = get(this.ceo, oldSupervisorPath) as Employee | undefined;
+          if (typeof oldSupervisor !== undefined) {
+            while (employee.subordinates.length > 0) {
+              const subordinate = employee.subordinates.pop() as Employee;
+              oldSupervisor?.subordinates.push(subordinate);
+            }
+          }
+        }
+
+        // Change the supervisor of the employee
+        const newSupervisorPath = this.findEmployee(this.ceo, supervisorID);
+        if (typeof newSupervisorPath === "string") {
+          const newSupervisor = get(this.ceo, newSupervisorPath) as Employee | undefined;
+          if (newSupervisor !== undefined) {
+            newSupervisor.subordinates.push(employee);
+          }
+        }
+      }
+    }
+
+
     this.moves.push(newMove);
   }
 
@@ -46,5 +84,7 @@ export class EmployeeOrgApp implements IEmployeeOrgApp {
 
 (() => {
   const app = new EmployeeOrgApp(Mark);
-  console.log(app.findEmployee(app.ceo, 4));
+  console.log(JSON.stringify(Mark));
+  app.move(12, 14);
+  console.log(JSON.stringify(app.ceo));
 })();
